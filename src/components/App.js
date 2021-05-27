d23531d1dbc87014a8de61ab279f7716830c78a9
Ravi Lamontagne <lamontagne.ravi@gmail.com>
2021-05-27 00:26:40 -0400

    Add Navbar and Nav Items

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import '../assets/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Alert from 'react-bootstrap/Alert';
+import Navigation from "./nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <Alert dismissible variant="danger">
-      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>
-        Change this and that and try again.
-      </p>
-    </Alert>
+    <Router>
+      <Navigation />
+    </Router>
   );
 }
 
---
+++ src/components/nav.js
@@ -0,0 +1,39 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Link from "react-router-dom/Link";
+
+const Navigation = () => {
+  return (
+    <Navbar expand={"lg"} className={"nav-bg"}>
+      <Navbar.Brand href="/">St. Lucia Elections</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className={"justify-content-end"}>
+        <Nav>
+          <Nav.Item>
+            <Link to={"/results"} className="nav-link">Results</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to={"/parties"} className="nav-link">Parties</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to={"/candidates"} className="nav-link">Candidates</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to={"/districts"} className="nav-link">Districts</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to={"/districts"} className="nav-link">Districts</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to={"/polling-stations"} className="nav-link">Polling Stations</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to={"/sections"} className="nav-link">Sections</Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Navigation;
