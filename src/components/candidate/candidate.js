80fcea1602c29f9a5f6f9b4300700687ec1f54be
Ravi Lamontagne <lamontagne.ravi@gmail.com>
2021-05-30 00:50:46 -0400

    Update Candidate page to use Cards

--- src/components/candidate/candidate.js
+++ src/components/candidate/candidate.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CandidateDataService from "../../services/candidate";
 import { Link } from "react-router-dom";
 import Loader from "react-loader-spinner";
+import {Col, Row, Card, Button, InputGroup, FormControl} from "react-bootstrap";
 
 export default class Candidate extends Component {
   constructor(props) {
@@ -9,16 +10,13 @@ export default class Candidate extends Component {
     this.onChangeSearchName = this.onChangeSearchName.bind(this);
     this.retrieveCandidates = this.retrieveCandidates.bind(this);
     this.refreshList = this.refreshList.bind(this);
-    this.setActiveCandidate = this.setActiveCandidate.bind(this);
     this.removeAllCandidates = this.removeAllCandidates.bind(this);
     this.searchName = this.searchName.bind(this);
     this.deleteCandidate = this.deleteCandidate.bind(this);
-    this.formatDate = this.formatDate.bind(this);
+    //this.formatDate = this.formatDate.bind(this);
 
     this.state = {
       candidates: [],
-      currentCandidate: null,
-      currentIndex: -1,
       searchName: "",
       isLoading: true
     };
@@ -55,13 +53,6 @@ export default class Candidate extends Component {
     });
   }
 
-  setActiveCandidate(candidate, index) {
-    this.setState({
-      currentCandidate: candidate,
-      currentIndex: index
-    });
-  }
-
   deleteCandidate() {
     CandidateDataService.delete(this.state.currentCandidate._id)
       .then(() => {
@@ -85,36 +76,28 @@ export default class Candidate extends Component {
       }).catch(e => { console.log(e) });
   }
 
-  formatDate(date) {
+  /*formatDate(date) {
     const formattedDate = new Date(date).toLocaleDateString();
 
     return (formattedDate);
-  }
+  }*/
 
   render() {
-    const { searchName, candidates, currentCandidate, currentIndex } = this.state;
+    const { searchName, candidates } = this.state;
 
     return (
       <div className="container list row">
         <div className="col-md-8">
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
+          <InputGroup className={"mb-3"}>
+            <FormControl
               placeholder="Search by Name"
               value={searchName}
               onChange={this.onChangeSearchName}
             />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary"
-                type="button"
-                onClick={this.searchName}
-              >
-                Search
-              </button>
-            </div>
-          </div>
+            <Button variant={"outline-secondary"} onClick={this.searchName}>
+              Search
+            </Button>
+          </InputGroup>
         </div>
         <div className="col-md-6">
           <h4>Candidate List</h4>
@@ -130,87 +113,39 @@ export default class Candidate extends Component {
                 d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
             </svg>
           </Link>
-          <button
-            className="btn btn-danger mb-2 mr-half"
-            onClick={this.removeAllCandidates}
-          >
+          <Button variant={"danger"} className={"mb-2 mr-half"} onClick={this.removeAllCandidates}>
             Remove All
-          </button>
-          <ul className="list-group">
-            <Loader
-              type={"MutatingDots"}
-              color={"Yellow"}
-              secondaryColor={"Red"}
-              visible={this.state.isLoading}
-            />
-            { candidates && candidates.map((candidate, index) => (
-              <li
-                className={
-                  "list-group-item " +
-                  (index === currentIndex ? "active" : "")
-                }
-                onClick={() => this.setActiveCandidate(candidate, index)}
-                key={index}
-              >
-                {candidate.name}
-              </li>
-            ))}
-          </ul>
+          </Button>
         </div>
-        <div className="col-md-6">
-          { currentCandidate ? (
-            <div>
-              <h4>Candidate Details</h4>
-              <div>
-                <label>
-                  <strong>Name:</strong>
-                </label>{" "}
-                { currentCandidate.name }
-              </div>
-              {/*<div>
-                <label>
-                  <strong>DOB:</strong>
-                </label>{" "}
-                { this.formatDate(currentCandidate.dob) }
-              </div>*/}
-              <div>
-                <label>
-                  <strong>Sex:</strong>
-                </label>{" "}
-                { currentCandidate.gender }
-              </div>
-              <div>
-                <label>
-                  <strong>Party:</strong>
-                </label>{" "}
-                { currentCandidate.party.name }
-              </div>
-              <div>
-                <label>
-                  <strong>District:</strong>
-                </label>{" "}
-                { currentCandidate.district.name }
-              </div>
-              <div>
-                <label>
-                  <strong>Comments:</strong>
-                </label>{" "}
-                { currentCandidate.comments }
-              </div>
-              <Link to={"/candidates/" + currentCandidate._id} className="btn btn-success mr-half">
-                Edit
-              </Link>
-              <button className="btn btn-danger mr-half" onClick={this.deleteCandidate}>
-                Delete
-              </button>
-            </div>
-          ) : (
-            <div>
-              <br />
-              <p>Please click on a Candidate...</p>
-            </div>
-          )}
-        </div>
+        <Row xs={1} md={2} lg={3} className={"g-3"}>
+          <Loader
+            type={"MutatingDots"}
+            color={"Yellow"}
+            secondaryColor={"Red"}
+            visible={this.state.isLoading}
+          />
+          {candidates && candidates.map((candidate) => (
+            <Col>
+              <Card>
+                <Card.Header>{candidate.name}</Card.Header>
+                <Card.Body>
+                  <Card.Text>
+                    <p><strong>Sex:{" "}</strong>{ candidate.gender }</p>
+                    <p><strong>Party:{" "}</strong>{ candidate.party.name }</p>
+                    <p><strong>District:{" "}</strong>{ candidate.district.name }</p>
+                    <p><strong>Comments:{" "}</strong>{ candidate.comments }</p>
+                  </Card.Text>
+                  <Link to={"/candidates/" + candidate._id} className={"btn btn-success mr-half"}>
+                    Edit
+                  </Link>
+                  <Button variant={"danger"} className={"mr-half"} onClick={this.deleteCandidate}>
+                    Delete
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
     );
   }
